68e0620b795bd274e8ea6dce441a0c2e8fa1b08f
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-10 16:38:58 +1200

    UI design - Add google fonts for Main heading

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './containers/Home/Home';
 function App() {
   return (
     <div className="App">
-          
         <Home/>
     </div>
   );
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-
+import './Home.css'
 import HeadingCard from "../../components/HeadingCard/HeadingCard";
 
-
-
 function Home() {
- 
   return (
-    <div>
+    <div className="home">
       <Header></Header>
-      <h1>Covid 19 Tracker</h1>
-        <HeadingCard></HeadingCard>
+      <h1 className="home__title">Covid-19 Tracker</h1>
+      <HeadingCard></HeadingCard>
     </div>
   );
 }
