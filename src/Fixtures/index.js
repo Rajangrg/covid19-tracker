37c7fb2c695f7674688672bbf967e8fce96fd75b
Rajan Gurung <rajangrg1@hotmail.com>
2020-08-11 16:12:20 +1200

    Added more API testing with Jest for Covid-19 API

--- src/Fixtures/index.js
+++ src/Fixtures/index.js
@@ -1,8 +1,27 @@
+import { Flag } from "semantic-ui-react";
+
 //set of sample objects for testing
 export const getWorldwideInfo = {
   cases: 500,
   deaths: 100,
   recovered: 200,
   active: 200,
-  };
-    
+};
+
+export const countryName = "New Zealand";
+
+export const getCountryNameInfo = {
+  country: "New Zealand",
+  cases: 500,
+  deaths: 100,
+  recovered: 200,
+  active: 200,
+};
+
+export const emptyCountryNameInfo = {
+  country: undefined,
+  cases: undefined,
+  deaths: undefined,
+  recovered: undefined,
+  active: undefined,
+};
--- src/__tests__/covid19_API.test.js
+++ src/__tests__/covid19_API.test.js
@@ -1,6 +1,13 @@
 import axios from "axios";
-import { getAllCountriesAPI } from "../service/disease_shAPI";
-import { getWorldwideInfo } from "../Fixtures/index";
+import {
+  getAllCountriesAPI,
+  getCountryInfoAPI,
+} from "../service/disease_shAPI";
+import {
+  getWorldwideInfo,
+  getCountryNameInfo,
+  emptyCountryNameInfo,
+} from "../Fixtures/index";
 
 jest.mock("axios");
 
@@ -8,13 +15,14 @@ describe("COVID-19 API", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
-  describe("worldwide covid-19 info", () => {
+
+  describe("worldwide covid-19 information", () => {
     it("request and get all worldwide total death, cases & recovered", async () => {
       //setup
       axios.get.mockImplementation(() =>
         Promise.resolve({ data: getWorldwideInfo })
       );
-      //work
+      //act
       const entity = await getAllCountriesAPI();
       //assertion/expects
       expect(axios.get).toHaveBeenCalledTimes(1);
@@ -22,6 +30,38 @@ describe("COVID-19 API", () => {
         "https://disease.sh/v3/covid-19/all"
       );
       expect(entity).toEqual(getWorldwideInfo);
+    });
+  });
+
+  describe("country information", () => {
+    it("request and get all country information", async () => {
+      //setup
+      axios.get.mockImplementation(() =>
+        Promise.resolve({ data: getCountryNameInfo })
+      );
+      //act
+      const entity = await getCountryInfoAPI("New Zealand");
+      //assertion/expects
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axios.get).toHaveBeenCalledWith(
+        `https://disease.sh/v3/covid-19/countries/New Zealand`
+      );
+      expect(entity).toEqual(getCountryNameInfo);
+    });
+
+    it("request and get empty information if country can't be found", async () => {
+      //setup
+      axios.get.mockImplementation(() =>
+        Promise.resolve({ data: emptyCountryNameInfo })
+      );
+      //act
+      const entity = await getCountryInfoAPI("N Zealand");
+      //assertion/expects
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axios.get).toHaveBeenCalledWith(
+        `https://disease.sh/v3/covid-19/countries/N Zealand`
+      );
+      expect(entity).toEqual(emptyCountryNameInfo);
     });
   });
 });
